04718a5d60d0d17ca5c9e0b47b05b56af1efe378
Drew Partridge <andrpart669@ed.amdsb.ca>
2022-10-31 10:26:18 -0400

    Add project files.

---
+++ LostAdvV.2/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace LostAdvV._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.opt1Button = new System.Windows.Forms.Button();
+            this.opt2Button = new System.Windows.Forms.Button();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(-1, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(489, 186);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "You\'re at a abandoned house";
+            // 
+            // opt1Button
+            // 
+            this.opt1Button.Location = new System.Drawing.Point(69, 267);
+            this.opt1Button.Name = "opt1Button";
+            this.opt1Button.Size = new System.Drawing.Size(75, 22);
+            this.opt1Button.TabIndex = 2;
+            this.opt1Button.Text = "-->";
+            this.opt1Button.UseVisualStyleBackColor = true;
+            this.opt1Button.Click += new System.EventHandler(this.opt1Button_Click);
+            // 
+            // opt2Button
+            // 
+            this.opt2Button.Location = new System.Drawing.Point(69, 377);
+            this.opt2Button.Name = "opt2Button";
+            this.opt2Button.Size = new System.Drawing.Size(75, 23);
+            this.opt2Button.TabIndex = 3;
+            this.opt2Button.Text = "-->";
+            this.opt2Button.UseVisualStyleBackColor = true;
+            this.opt2Button.Click += new System.EventHandler(this.opt2Button_Click);
+            // 
+            // option1Label
+            // 
+            this.option1Label.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.Location = new System.Drawing.Point(150, 267);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(133, 23);
+            this.option1Label.TabIndex = 4;
+            this.option1Label.Text = "Leave";
+            // 
+            // option2Label
+            // 
+            this.option2Label.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.Location = new System.Drawing.Point(150, 377);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(133, 23);
+            this.option2Label.TabIndex = 5;
+            this.option2Label.Text = "Go in";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.BackgroundImage = global::LostAdvV._2.Properties.Resources.abandoned_house;
+            this.ClientSize = new System.Drawing.Size(1049, 664);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.opt2Button);
+            this.Controls.Add(this.opt1Button);
+            this.Controls.Add(this.outputLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Abandoned House";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button opt1Button;
+        private System.Windows.Forms.Button opt2Button;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+    }
+}
+
---
+++ LostAdvV.2/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LostAdvV._2
+{
+    public partial class Form1 : Form
+    {
+
+        Random randNum = new Random();
+
+        int page = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void opt2Button_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (page == 1)
+            {
+                page = 3;
+                this.BackgroundImage = Properties.Resources.Stairs;
+            }
+            else if (page == 2)
+            {
+                page = 1;
+                this.BackgroundImage = Properties.Resources.abandoned_house;
+
+            }
+            else if (page == 3)
+            {
+                page = 5;
+                this.BackgroundImage = Properties.Resources.Stairs;
+
+            }
+            else if (page == 4)
+            {
+                page = 1;
+                this.BackgroundImage = Properties.Resources.abandoned_house;
+
+            }
+            else if (page == 5)
+            {
+                page = 7;
+                this.BackgroundImage = Properties.Resources.Map;
+
+            }
+            else if (page == 6)
+            {
+                page = 1;
+                this.BackgroundImage = Properties.Resources.abandoned_house;
+
+
+            }
+            else if (page == 7)
+            {
+
+                randNum.Next(1, 7);
+                int random = randNum.Next();
+                if (random == 1)
+
+                {
+                    page = 11;
+                    this.BackgroundImage = Properties.Resources.abandoned_house;
+
+                }
+                else
+                {
+                    page = 9;
+                    this.BackgroundImage = Properties.Resources.Tresure;
+
+                }
+            }
+            else if (page == 8)
+            {
+                page = 1;
+                this.BackgroundImage = Properties.Resources.abandoned_house;
+
+            }
+            else if (page == 9)
+            {
+                page = 1;
+                this.BackgroundImage = Properties.Resources.abandoned_house;
+
+            }
+            else if (page == 11)
+            {
+                page = 1;
+                this.BackgroundImage = Properties.Resources.abandoned_house;
+
+            }
+
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "you are at an abandoned house";
+                    option1Label.Text = "leave ";
+                    option2Label.Text = "go in";
+                    break;
+                case 2:
+                    outputLabel.Text = " You  turn around and leave. sadly mike tyson is waiting for you and knocks you out. you lose. play again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 3:
+                    outputLabel.Text = "you go into the house and find a weird flight of stairs. Climb them?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+                case 4:
+                    outputLabel.Text = "you decide to not go down the stairs. u turn around and Danial Larusso crane kicks you in the face. you lose. play again? ";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 5:
+                    outputLabel.Text = "You go down the stairs and find a weird looking book. open it?";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 6:
+                    outputLabel.Text = "You Decide too put the book down and carry on. Conner McGregor is there waiting for you and fly kicks you in the head . You lose. Play again? ";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 7:
+                    outputLabel.Text = "You opened the book. you found a treasure map. follow it? ";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 8:
+                    outputLabel.Text = "You Decide too put the map down and carry on. but, as you put the map down Bruce lee hit u with the one inch punch. You lose. Play again? ";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 9:
+                    outputLabel.Text = "you followed the map and it led you too the most money you would ever need. You win! play again?";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 11:
+                    outputLabel.Text = "You followed the map and it led you back out of the house. you havent gained anything but atleast you're alive. wanna go back inside?";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thank you for playing!";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    Thread.Sleep(2000);
+                    Refresh();
+                    Application.Exit();
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void opt1Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+                this.BackgroundImage = Properties.Resources.Mike_Tyson;
+            }
+            else if (page == 2)
+            {
+                page = 99;
+            }
+            else if (page == 3)
+            {
+                page = 4;
+                this.BackgroundImage = Properties.Resources.Danial;
+            }
+            else if (page == 4)
+            {
+                page = 99;
+            }
+            else if (page == 5)
+            {
+                page = 6;
+                this.BackgroundImage = Properties.Resources.connor;
+            }
+            else if (page == 6)
+            {
+                page = 99;
+            }
+            else if (page == 7)
+            {
+
+                randNum.Next(1, 7);
+                int random = randNum.Next();
+                if (random == 1)
+
+                {
+                    this.BackgroundImage = Properties.Resources.abandoned_house;
+                    page = 11;
+                }
+                else
+                {
+                    page = 9;
+                    this.BackgroundImage = Properties.Resources.Tresure;
+                }
+            }
+            else if (page == 8)
+            {
+                page = 99;
+            }
+            else if (page == 9)
+            {
+                page = 99;
+            }
+            else if (page == 11)
+            {
+                page = 99;
+            }
+
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "you are at an abandoned house";
+                    option1Label.Text = "leave ";
+                    option2Label.Text = "go in";
+                    break;
+                case 2:
+                    outputLabel.Text = " You  turn around and leave. sadly mike tyson is waiting for you and knocks you out. you lose. play again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 3:
+                    outputLabel.Text = "you go into the house and find a weird flight of stairs. Climb them?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+                case 4:
+                    outputLabel.Text = "you decide to not go down the stairs. u turn around and Danial Larusso crane kicks you in the face. you lose. play again? ";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 5:
+                    outputLabel.Text = "You go down the stairs and find a weird looking book. open it?";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 6:
+                    outputLabel.Text = "You Decide too put the book down and carry on. Conner McGregor is there waiting for you and fly kicks you in the head . You lose. Play again? ";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 7:
+                    outputLabel.Text = "You opened the book. you found a treasure map. follow it? ";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 8:
+                    outputLabel.Text = "You Decide too put the map down and carry on. but, as you put the map down Bruce lee hit u with the one inch punch. You lose. Play again? ";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 9:
+                    outputLabel.Text = "you followed the map and it led you too the most money you would ever need. You win! play again?";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 11:
+                    outputLabel.Text = "You followed the map and it led you back out of the house. you havent gained anything but atleast you're alive. wanna go back inside?";
+                    option1Label.Text = "No";
+                    option2Label.Text = "Yes";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thank you for playing!";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    Thread.Sleep(2000);
+                        Refresh();
+                    Application.Exit();
+
+                    break;
+                default:
+                    break;
+
+
+            }
+        }
+
+    }
+}
